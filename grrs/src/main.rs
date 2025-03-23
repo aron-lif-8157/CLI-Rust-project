4a4e38a03b2ee034a77bde2397acd0fc10fda0bb
Aron Lif <aron.lif.8157@student.uu.se>
2025-03-23 16:08:03 +0100

    done with testing

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,18 +1,20 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+use grrs::find_matches;
+
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
-fn main() {
 
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(||format!("could not read file `{}`", args.path.display()))?;
 
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
 }
+
--- grrs/target/debug/grrs.d
+++ grrs/target/debug/grrs.d
@@ -1,1 +1,1 @@
-/home/aron/Documents/Project/CLI-project/grrs/target/debug/grrs: /home/aron/Documents/Project/CLI-project/grrs/src/main.rs
+/home/aron/Documents/Project/CLI-project/grrs/target/debug/grrs: /home/aron/Documents/Project/CLI-project/grrs/src/lib.rs /home/aron/Documents/Project/CLI-project/grrs/src/main.rs
